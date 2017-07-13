589bd38524b9257d151e26224fd4325a06ed84be
luisesh11 <luisesh1x10@hotmail.com>
2017-07-13 15:16:10 +0000

    remedio temporar para los folios

--- app/pdf/recibo_pdf.rb
+++ app/pdf/recibo_pdf.rb
@@ -13,7 +13,7 @@ class ReciboPdf < Prawn::Document
       rectangle [50,750+des], 500, 170
       
       image "#{Rails.root}/app/assets/images/logo marjo.png", width: 155, height: 45, :at => [70,740+des]
-      draw_text  "FOLIO:  #{ticket.id}",:size=>9, :at => [400,720+des]
+      draw_text  "FOLIO:  2503616176B#{"0"*(5-ticket.id.to_s.length)+ticket.id.to_s}",:size=>9, :at => [400,720+des]
       draw_text  "FECHA CARGO: #{ticket.payment.fecha_de_pago}" ,:size=>9, :at => [400,710+des]
       draw_text  "SUCURSAL: #{ticket.payment.credit.padre.branch_office.nombre}",:size=>9, :at => [400,700+des]
       draw_text  "AGENTE: #{ticket.payment.credit.padre.clave}",:size=>9, :at => [400,690+des]
@@ -36,42 +36,42 @@ class ReciboPdf < Prawn::Document
       rectangle [50,750+des], 500, 170
       
       image "#{Rails.root}/app/assets/images/logo marjo.png", width: 155, height: 45, :at => [70,740+des]
-      draw_text  "FOLIO:  #{ticket.id}",:size=>9, :at => [400,720+des]
+      draw_text  "FOLIO:  2503616176B#{"0"*(5-ticket.id.to_s.length)+ticket.id.to_s}",:size=>9, :at => [400,720+des]
       draw_text  "FECHA CARGO: #{ticket.payment.fecha_de_pago}" ,:size=>9, :at => [400,710+des]
       draw_text  "SUCURSAL: #{ticket.payment.credit.padre.branch_office.nombre}",:size=>9, :at => [400,700+des]
       draw_text  "AGENTE: #{ticket.payment.credit.padre.clave}",:size=>9, :at => [400,690+des]
   
       
-      draw_text  "Copia 1 ", :at => [400,730+des]
+      draw_text  "Original ", :at => [400,730+des]
       
-      draw_text  "RECIBO DE :#{ticket.payment.fecha_de_pago} ",:size=>9, :at => [70,670+des]
+      draw_text  "RECIBO DE : #{ticket.payment.credit.nombre_completo_deudor} ",:size=>9, :at => [70,670+des]
       draw_text  "LA CANTIDAD DE:  #{Dinero.to_money(ticket.cantidad)}",:size=>9, :at => [70,660+des]
-      draw_text  "                     (#{I18n.with_locale(:es) { (ticket.cantidad).to_i.to_words }} PESOS /100 M.N.)",:size=>9, :at => [70,650+des]
+      draw_text  "                     (#{(I18n.with_locale(:es) { (ticket.cantidad).to_i.to_words }).upcase } PESOS 00/100 M.N.)",:size=>9, :at => [70,650+des]
       draw_text  "POR CONCEPTO DE: PAGO O ABONO AL CARGO QUINCENAL #{ticket.payment.recibo} CORRESPONDIENTE AL CREDITO",:size=>9, :at => [70,640+des]
       draw_text  "                  OTORGADO EL #{ticket.payment.credit.fecha}",:size=>9, :at => [70,630+des]
-      draw_text  "ATRASADO ACTUAL DE: #{"0"} ",:size=>9, :at => [70,620+des]
+      draw_text  "ATRASADO ACTUAL DE: #{Dinero.to_money at} ",:size=>9, :at => [70,620+des]
       
       draw_text  "_________________________", :at => [370,610+des]
       draw_text  "FIRMA CHEQUE RECIBIDO",:size=>8, :at => [400,600+des]
       
         des = -400
-       rectangle [50,750+des], 500, 170
+      rectangle [50,750+des], 500, 170
       
       image "#{Rails.root}/app/assets/images/logo marjo.png", width: 155, height: 45, :at => [70,740+des]
-      draw_text  "FOLIO:  #{ticket.id}",:size=>9, :at => [400,720+des]
+      draw_text  "FOLIO:  2503616176B#{"0"*(5-ticket.id.to_s.length)+ticket.id.to_s}",:size=>9, :at => [400,720+des]
       draw_text  "FECHA CARGO: #{ticket.payment.fecha_de_pago}" ,:size=>9, :at => [400,710+des]
       draw_text  "SUCURSAL: #{ticket.payment.credit.padre.branch_office.nombre}",:size=>9, :at => [400,700+des]
       draw_text  "AGENTE: #{ticket.payment.credit.padre.clave}",:size=>9, :at => [400,690+des]
   
       
-      draw_text  "Copia 2 ", :at => [400,730+des]
+      draw_text  "Original ", :at => [400,730+des]
       
-      draw_text  "RECIBO DE :#{ticket.payment.fecha_de_pago} ",:size=>9, :at => [70,670+des]
+      draw_text  "RECIBO DE : #{ticket.payment.credit.nombre_completo_deudor} ",:size=>9, :at => [70,670+des]
       draw_text  "LA CANTIDAD DE:  #{Dinero.to_money(ticket.cantidad)}",:size=>9, :at => [70,660+des]
-      draw_text  "                     (#{I18n.with_locale(:es) { (ticket.cantidad).to_i.to_words }} PESOS /100 M.N.)",:size=>9, :at => [70,650+des]
+      draw_text  "                     (#{(I18n.with_locale(:es) { (ticket.cantidad).to_i.to_words }).upcase } PESOS 00/100 M.N.)",:size=>9, :at => [70,650+des]
       draw_text  "POR CONCEPTO DE: PAGO O ABONO AL CARGO QUINCENAL #{ticket.payment.recibo} CORRESPONDIENTE AL CREDITO",:size=>9, :at => [70,640+des]
       draw_text  "                  OTORGADO EL #{ticket.payment.credit.fecha}",:size=>9, :at => [70,630+des]
-      draw_text  "ATRASADO ACTUAL DE: #{"0"} ",:size=>9, :at => [70,620+des]
+      draw_text  "ATRASADO ACTUAL DE: #{Dinero.to_money at} ",:size=>9, :at => [70,620+des]
       
       draw_text  "_________________________", :at => [370,610+des]
       draw_text  "FIRMA CHEQUE RECIBIDO",:size=>8, :at => [400,600+des]
