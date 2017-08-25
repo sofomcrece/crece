dcbb0767ff64a64278189a648f87c4af21b1a1f7
luisesh11 <luisesh1x10@hotmail.com>
2017-08-25 22:30:33 +0000

    cambio en se seguimiento

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -65,15 +65,8 @@ class ReportsController < ApplicationController
     tabla = []
     credits = padre.credits.where(product:producto.to_i).where(status:1)
    credits.each do |credit|
-    payment  = Payment.all.where("credit_id = ? and fecha_de_pago = ?", credit.id, fecha)[0]
-    activar = false
-    if(payment.nil?)
-        activar = false
-    elsif payment.estatus==2
-        activar= true
-    else
-        activar= false
-    end
+    payment  = Payment.all.where("credit_id = ? and fecha_de_corte = ?", credit.id, fecha)[0]
+    
     fila = Hash.new()
     fila["nombre_completo"] = "#{credit.nombre_1} #{credit.nombre_2} #{credit.apellido_paterno} #{credit.apellido_materno}"
     fila["fecha"] = credit.fecha
@@ -81,17 +74,17 @@ class ReportsController < ApplicationController
     fila["monto_a_pagar"] = credit.payments.sum(:importe)
     fila["pagado"] = Ticket.joins(:payment=>:credit).where("credits.id = ? and tickets.status = ?",credit.id,0).sum(:cantidad)
     fila["adeudo"] = fila["monto_a_pagar"].to_s.to_d - fila["pagado"].to_s.to_d
-    fila["pagar"] = Payment.all.where("credit_id = ? and fecha_de_pago = ?", credit.id, fecha).sum(:importe).to_s.to_d
-    pagos = Payment.all.where("credit_id = ? and fecha_de_pago < ?", credit.id, fecha)
+    fila["pagar"] = Payment.all.where("credit_id = ? and fecha_de_corte = ?", credit.id, fecha).sum(:importe).to_s.to_d - Payment.joins(:tickets).where("credit_id = ? and fecha_de_corte = ? and tickets.status = 0 and tickets.created_at < ?", credit.id, fecha,fecha).sum(:cantidad)
+    pagos = Payment.all.where("credit_id = ? and fecha_de_corte < ?", credit.id, fecha)
     fila["atrasado"] = pagos.sum(:importe).to_s.to_d <= fila["pagado"].to_s.to_d ? 0 : pagos.sum(:importe).to_s.to_d - fila["pagado"].to_s.to_d
-    fila["interes_moratorio"] = pagos.sum(:interes).to_s.to_d
-    fila["total_a_cobrar"] = (activar)? "0" : fila["interes_moratorio"] + fila["atrasado"] + fila["pagar"]
-    fila["cobrado"] =(activar)? "0" : Payment.joins(:tickets).where("credit_id = ? and fecha_de_pago = ? and tickets.status = 0 and tickets.created_at >= ? ", credit.id, fecha,fecha).sum(:cantidad)
+    fila["interes_moratorio"] = Payment.where("credit_id = ? and fecha_de_corte <= ? and estatus != ? ", credit.id, fecha,2).sum(:interes).to_s.to_d
+    fila["total_a_cobrar"] =  fila["interes_moratorio"] + fila["atrasado"] + fila["pagar"]
+    fila["cobrado"] = Payment.joins(:tickets).where("credit_id = ? and fecha_de_corte = ? and tickets.status = 0 and tickets.created_at >= ? ", credit.id, fecha,fecha).sum(:cantidad)
     fila["diferencia"] = fila["total_a_cobrar"].to_s.to_d - fila["cobrado"].to_s.to_d
-    fila["adelantado"] = Ticket.joins(:payment=>:credit).where("credits.id = ? and payments.fecha_de_pago > ? and tickets.status = ?",credit.id, fecha,0).sum(:cantidad)
+    fila["adelantado"] = Ticket.joins(:payment=>:credit).where("credits.id = ? and payments.fecha_de_corte > ? and tickets.status = ?",credit.id, fecha,0).sum(:cantidad)
     fila["empresa"] = credit.padre.nombre_completo
-    fila["numero_de_pago"] = Payment.all.where("credit_id = ? and fecha_de_pago = ?", credit.id, fecha)[0].recibo unless Payment.all.where("credit_id = ? and fecha_de_pago = ?", credit.id, fecha)[0].nil?
-    fila["numero_de_creditos"] = credit.customer.credits.count
+    fila["numero_de_pago"] = Payment.all.where("credit_id = ? and fecha_de_corte = ?", credit.id, fecha)[0].recibo unless Payment.all.where("credit_id = ? and fecha_de_corte = ?", credit.id, fecha)[0].nil?
+    fila["numero_de_creditos"] = credit.customer.credits.where("credits.status = ? or credits.status = ? ",1,3).count
     tabla << fila
    end
    return tabla
--- app/views/products/show.json.jbuilder
+++ app/views/products/show.json.jbuilder
@@ -1,6 +1,6 @@
 json.extract! @product, :id, :nombre_del_producto, :registro_del_contrado_de_adhesion, :plazo_de_prestamo, :numero_de_pagos_a_realizar, :taza_de_interes_ordinaria, :taza_de_interes_moratoria, :cat_sin_iva, :numero_de_meses_de_sueldo_para_prestamo, :created_at, :updated_at
 fa = Payment.select(:fecha_de_impresion).joins(:credit=>:product).where("credits.status = ? ",1 ).where("products.id = ? ", @product.id).uniq.order(:fecha_de_impresion)
-fp = Payment.select(:fecha_de_pago).joins(:credit=>:product).where("credits.status = ? ",1 ).where("products.id = ? ", @product.id).uniq.order(:fecha_de_pago)
+fp = Payment.select(:fecha_de_pago,:fecha_de_corte).joins(:credit=>:product).where("credits.status = ? ",1 ).where("products.id = ? ", @product.id).uniq.order(:fecha_de_pago)
 json.fechas_activas fa
 json.fechas_de_pago fp
 json.fechas_activas_con_formato  json.array(fa) do |f|
@@ -8,4 +8,5 @@ json.fechas_activas_con_formato  json.array(fa) do |f|
 end
 json.fechas_de_pago_con_formato json.array(fp) do |f|
     json.fecha_de_pago f.fecha_de_pago.strftime("%d/%m/%Y")
+    json.fecha_de_corte f.fecha_de_corte
 end
