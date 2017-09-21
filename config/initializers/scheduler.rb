6f5210abc8b2a61c11f2b2e3e29c12fa63fcda4f
luisesh11 <luisesh1x10@hotmail.com>
2017-09-21 14:10:29 -0600

    se agusto  fecha de nacimineto y tambien se cargo un prueba de rufus

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,9 +1,12 @@
-require 'rubygems'
+
 require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
-scheduler.cron '0 22 * * *' do
-  Coman.create(c:"#{Time.now} se creo")
+unless scheduler.down?
+  scheduler.cron '0 22 * * *' do
+    Coman.create(c:"#{Time.now} yaaaa jala")
+  end
+  scheduler.every("300s") do
+    Coman.create(c:"#{Time.now} abierto")
+  end
 end
-scheduler.interval '10s' do
-  Coman.create(c:"I assure you! It's #{Time.now}")
-end
+
