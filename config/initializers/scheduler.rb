a62d9e1076844d3f6db1b15f70aa1ec439de4488
luisesh11 <luisesh1x10@hotmail.com>
2017-09-27 02:55:38 -0600

    rufus

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -10,33 +10,3 @@
 #  end
 #end
 
-def rufus_running?(s)
-  return false unless s.up?
-  return false unless s.thread.alive?
-  true
-end
-
-def is_rufus_scheduler_already_running_in_my_rails_application?
-
-  return false unless defined?(Rufus::Scheduler)
-
-  ObjectSpace.each_object do |o|
-    next unless o.is_a?(Rufus::Scheduler)
-    return true if rufus_running?(o)
-  end
-
-  false
-end
-alias rufus? is_rufus_scheduler_already_running_in_my_rails_application?
-
-
-require 'rufus-scheduler'
-scheduler = Rufus::Scheduler.new
-if rufus?
-  scheduler.cron '0 22 * * *' do
-    Coman.create(c:"#{Time.now} yaaaa jala")
-  end
-  scheduler.every("10s") do
-    Coman.create(c:"#{Time.now} abierto")
-  end
-end
