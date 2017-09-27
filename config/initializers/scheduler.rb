430ddef474275bf7527e7cf798e5abb4352b5225
luisesh11 <luisesh1x10@hotmail.com>
2017-09-27 02:02:22 -0600

    rufus

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,12 +1,18 @@
 
+#require 'rufus-scheduler'
+#scheduler = Rufus::Scheduler.new
+#unless scheduler.down?
+#  scheduler.cron '0 22 * * *' do
+#    Coman.create(c:"#{Time.now} yaaaa jala")
+#  end
+#  scheduler.every("60s") do
+#    Coman.create(c:"#{Time.now} abierto")
+#  end
+#end
 require 'rufus-scheduler'
-scheduler = Rufus::Scheduler.new
-unless scheduler.down?
-  scheduler.cron '0 22 * * *' do
-    Coman.create(c:"#{Time.now} yaaaa jala")
-  end
-  scheduler.every("60s") do
-    Coman.create(c:"#{Time.now} abierto")
-  end
+scheduler = Rufus::Scheduler.start_new
+job = scheduler.every '60s' do
+  Coman.create(c:"#{Time.now} nuevo intento")
 end
+scheduler.start
 
