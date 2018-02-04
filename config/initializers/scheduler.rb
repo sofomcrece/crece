b470c3e0667f1576f707d7a7f47494a2bcf9973e
luisesh11 <luisesh1x10@hotmail.com>
2018-02-04 06:37:42 -0700

    ds

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,12 +1,14 @@
 
 require 'rufus-scheduler'
-scheduler = Rufus::Scheduler.singleton
+
+
+scheduler = Rufus::Scheduler::singleton
 unless scheduler.down?
 #  scheduler.cron '0 22 * * *' do
 #    Coman.create(c:"#{Time.now} yaaaa jala")
 #  end
   scheduler.every("1m", :blocking => true) do
-    Expire.create(comentarios:"1vecimiento de prssueba3 #{Auxiliar.fecha_natural Time.now}",fecha:Time.now, control:(Expire.last.nil?)? 1 : Expire.last.id + 1 )
+    Expire.create(comentarios:"2vecimiento de prssueba3 #{Auxiliar.fecha_natural Time.now}",fecha:Time.now, control:(Expire.last.nil?)? 1 : Expire.last.id + 1 )
   end
 end
 
