a364bbf92f28a34679952f96f8d3252380e0a745
luisesh11 <luisesh1x10@hotmail.com>
2018-02-04 06:23:30 -0700

    d

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -5,8 +5,8 @@ unless scheduler.down?
 #  scheduler.cron '0 22 * * *' do
 #    Coman.create(c:"#{Time.now} yaaaa jala")
 #  end
-  scheduler.every("1m") do
-    Expire.create(comentarios:"vecimiento de prssueba3 #{Auxiliar.fecha_natural Time.now}",fecha:Time.now, control:(Expire.last.nil?)? 1 : Expire.last.id + 1 )
+  scheduler.every("1m", :blocking => true) do
+    Expire.create(comentarios:"1vecimiento de prssueba3 #{Auxiliar.fecha_natural Time.now}",fecha:Time.now, control:(Expire.last.nil?)? 1 : Expire.last.id + 1 )
   end
 end
 
