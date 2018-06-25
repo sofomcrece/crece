5982841b7ff4b851d58b4688abf8bad20f40feb8
luisesh11 <luisesh1x10@hotmail.com>
2018-06-25 05:09:59 -0600

    quitando rufus cron

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -7,8 +7,8 @@ unless scheduler.down?
 #  scheduler.cron '0 22 * * *' do
 #    Coman.create(c:"#{Time.now} yaaaa jala")
 #  end
-  scheduler.every("1m", :blocking => true) do
-    Coman.create(c:Coman.last.c.to_i + 1)
-  end
+#  scheduler.every("1m", :blocking => true) do
+#    Coman.create(c:Coman.last.c.to_i + 1)
+#  end
 end
 
