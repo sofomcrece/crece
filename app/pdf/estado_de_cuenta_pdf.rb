723a193ce24187deea94e2eac801aec9d6d30501
luisesh11 <luisesh1x10@hotmail.com>
2019-01-16 07:35:46 -0700

    leyenda pdf

--- app/pdf/estado_de_cuenta_pdf.rb
+++ app/pdf/estado_de_cuenta_pdf.rb
@@ -2,12 +2,19 @@ class EstadoDeCuentaPdf < Prawn::Document
   def initialize(r,fechas)
     super(:margin => [50,50,50,50])
     @customer=r
+    base_size = 7
     @f1 = fechas[0]
     @f2 = fechas[1]
     @credits = @customer.credits.select(:id,:monto_solicitud,:fecha_de_contrato,:product_id)
     @credits  = @credits.select(:id).where("credits.fecha_de_contrato > ?",@f1.to_date) unless @f1.nil?
     @credits  = @credits.select(:id).where("credits.fecha_de_contrato < ?",@f2.to_date) unless @f2.nil?
     text_content
+    page_count.times do |i|
+      go_to_page(i+1)
+      draw_text  "*CAT (COSTO ANUAL TOTAL) 115.82% SIN IVA" ,:size=>base_size, :at => [-10, bounds.bottom + 0]
+      draw_text  "*CONDUSEF" ,:size=>base_size, :at => [-10, bounds.bottom + -8]
+      draw_text  "COMISION PARA LA PROTECCION Y LA DEFENSA DE USUARIOS DE SERVICIOS FINANCIEROS. TEL (55) 5340 0999 8080 Y 01 800 999 8080 www.condusef.gob.mx" ,:size=>base_size, :at => [-10, bounds.bottom - 16]
+    end
   end
   def text_content
     base_size = 9
