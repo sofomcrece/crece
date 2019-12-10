c098ca7efe18cbe604d0a58466f889fad25f479b
luisesh1 <agonzalez@quisa.com.mx>
2019-12-10 11:15:47 -0700

    xtickets

--- app/models/auxiliar.rb
+++ app/models/auxiliar.rb
@@ -233,9 +233,9 @@ class Auxiliar < ActiveRecord::Base
         xmonto_a_pagar = credit.payments.sum(:importe)
         xpagado = Ticket.joins(:payment=>:credit).where("credits.id = ? and tickets.status = ?",credit.id,0).sum(:cantidad)
         xadeudo = xmonto_a_pagar - xpagado
-        xultpago = Ticket.joins(:payment=>:credit).where("credits.id = ? and tickets.status = ?",credit.id,0).order("tickets.updated_at").last
-        next if xadeudo == 0 and xultpago.tickets.updated_at.to_date > fecha.to_date
-         
+        xtickets = Ticket.joins(:payment=>:credit).where("credits.id = ? and tickets.status = ?",credit.id,0).order("tickets.updated_at").last
+        xultpago = xtickets.updated_at.to_date
+        next if xadeudo == 0 and xultpago > fecha
         #next if credit.payments.sum(:importe).to_s.to_d - (Ticket.joins(:payment=>:credit).where("credits.id = ? and tickets.status = ?",credit.id,0).sum(:cantidad)) = 0
         fila = Hash.new()
         fila["nombre_completo"] = "#{credit.nombre_completo_deudor}"
