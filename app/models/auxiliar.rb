5fb0a1f611aa61d11dde65d59666a80e5047065e
luisesh1 <agonzalez@quisa.com.mx>
2019-12-11 10:59:54 -0700

    marcha atras a seguim adeudo=0

--- app/models/auxiliar.rb
+++ app/models/auxiliar.rb
@@ -230,11 +230,11 @@ class Auxiliar < ActiveRecord::Base
           tabla << self.generador_de_tuplas(credit,fecha)
           next
         end
-        xmonto_a_pagar = credit.payments.sum(:importe)
-        xpagado = Ticket.joins(:payment=>:credit).where("credits.id = ? and tickets.status = ?",credit.id,0).sum(:cantidad)
-        xadeudo = xmonto_a_pagar - xpagado
-        #xultpago = Ticket.joins(:payment=>:credit).where("credits.id = ? and tickets.status = ?",credit.id,0).order("tickets.updated_at").last
-        next if xadeudo == 0 and seguimiento.atrasado == 0
+        #xmonto_a_pagar = credit.payments.sum(:importe)
+        #xpagado = Ticket.joins(:payment=>:credit).where("credits.id = ? and tickets.status = ?",credit.id,0).sum(:cantidad)
+        #xadeudo = xmonto_a_pagar - xpagado
+        
+        #next if xadeudo == 0 and seguimiento.atrasado == 0
 
         #next if credit.payments.sum(:importe).to_s.to_d - (Ticket.joins(:payment=>:credit).where("credits.id = ? and tickets.status = ?",credit.id,0).sum(:cantidad)) = 0
         fila = Hash.new()
