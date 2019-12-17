9cb9fb7f5d51c622797e19214ebefa010473f676
luisesh1 <agonzalez@quisa.com.mx>
2019-12-16 18:56:05 -0700

    adeudo=0

--- app/models/auxiliar.rb
+++ app/models/auxiliar.rb
@@ -231,15 +231,11 @@ class Auxiliar < ActiveRecord::Base
           next
         end
 
-        #xmonto_a_pagar = credit.payments.sum(:importe)
-        #xpagado = Ticket.joins(:payment=>:credit).where("credits.id = ? and tickets.status = ?",credit.id,0).sum(:cantidad)
-        #xadeudo = xmonto_a_pagar - xpagado
+        xmonto_a_pagar = credit.payments.sum(:importe)
+        xpagado = Ticket.joins(:payment=>:credit).where("credits.id = ? and tickets.status = ? and tickets.updated_at.to_date<fecha.to_date",credit.id,0).sum(:cantidad)
+        xadeudo = xmonto_a_pagar - xpagado
         
-        #next if xadeudo == 0 and seguimiento.atrasado == 0
-
-
-        xultpago=Ticket.joins(:payment=>:credit).where("credits.id = ? and tickets.status = ?",credit.id,0).order("updated_at").last.updated_at.to_date
-        next if credit.status == 3
+        next if xadeudo == 0 
 
         #next if credit.payments.sum(:importe).to_s.to_d - (Ticket.joins(:payment=>:credit).where("credits.id = ? and tickets.status = ?",credit.id,0).sum(:cantidad)) = 0
         fila = Hash.new()
