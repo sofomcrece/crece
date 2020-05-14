c292acdb4c936e24ba386df7ab1ec60ea0f8717e
luisesh1 <agonzalez@quisa.com.mx>
2020-05-14 17:04:31 +0000

    Modif_seguim lo cobrado

--- app/models/product.rb
+++ app/models/product.rb
@@ -81,14 +81,14 @@ class Product < ActiveRecord::Base
           creditos = self.credits.where(status:1, referencia_agente_empresa:60).order(:apellido_paterno)
           return if Auxiliar.seguimiento_guardado_contador(creditos,fechainput) == 0
           tuplas = Auxiliar.seguimiento_por_creditos_guardados(creditos, fechainput)
-          tuplas.each do |t|
-               Seguimiento.update(
+          #tuplas.each do |t|
+          #     Seguimiento.update(
                     
-                    cobrado:t["cobrado"], 
-                    diferencia:t["diferencia"]
+          #          cobrado:t["cobrado"], 
+          #          diferencia:t["diferencia"]
                     
-               )
-          end
+          #     )
+          #end
      end
      def almacenar_seguimientos(fechainput)
           creditos = self.credits.where(status:1).order(:apellido_paterno)
