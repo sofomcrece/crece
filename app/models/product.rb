0894d8516d1eeb0225160238d305c977097390f6
luisesh1 <agonzalez@quisa.com.mx>
2020-05-29 18:06:16 +0000

    Correccion ModSeguim8

--- app/models/product.rb
+++ app/models/product.rb
@@ -87,8 +87,7 @@ class Product < ActiveRecord::Base
                          seg = Seguimiento.where(credit_id:3330, fecha_corte:fechainput)
 
                          seg.each do |s|
-                              s.update(cobrado:s["cobrado"])
-                              s.save
+                              return s["cobrado"]
                               #s.modif
                               #return s["credit_id"]
                               #return xc
