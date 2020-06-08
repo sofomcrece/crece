46aaf88e49b1833d3f3c5c9cbc0f321a0ff3ee84
luisesh1 <agonzalez@quisa.com.mx>
2020-06-08 15:45:29 +0000

    Correccion Seguim Cobrado mayor que 0

--- app/models/auxiliar.rb
+++ app/models/auxiliar.rb
@@ -296,8 +296,9 @@ class Auxiliar < ActiveRecord::Base
         xpagado=Ticket.joins(:payment=>:credit).where("credits.id = ? and tickets.status = ?",credit.id,0).sum(:cantidad).to_s.to_d
         xtotalDeuda=xmonto_a_pagar.to_s.to_d - xpagado.to_s.to_d
         xtotalPagar=seguimiento.a_pagar.to_s.to_d
+        xlocobrado=Ticket.where(updated_at:credit.product.rangoDeCorte(fecha)).joins(:payment=>:credit).where("tickets.status = ?",0).where("payments.credit_id = ?",credit.id).sum("tickets.cantidad") - fila["adelantado"]
 
-        if xtotalDeuda==0 and xtotalPagar==0
+        if xtotalDeuda==0 and xtotalPagar==0 and xlocobrado==0
           #fila = Hash.new()
           #return fila
         else
