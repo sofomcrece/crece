fe13e865c7dd7953d4163cd5c6ffc758a90a9e95
EC2 Default User <ec2-user@ip-172-31-12-50.ec2.internal>
2021-11-23 16:35:08 +0000

    2 corridas

--- app/pdf/corrida_pdf.rb
+++ app/pdf/corrida_pdf.rb
@@ -27,6 +27,21 @@ class CorridaPdf < Prawn::Document
           ["CAT SIN IVA","#{'%.2f' % @credit.cat_sin_iva}%"]],:cell_style => { size: 8 })
     move_down 20
     table(@arreglo,:cell_style => { size: 8 })
+    start_new_page 
+
+    image "#{Rails.root}/app/assets/images/logo marjo.png", width: 155, height: 45
+    text "#{@credit.nombre_completo_deudor}"
+    table([["FECHA DE PRESTAMO", @credit.fecha_de_contrato.strftime("%d/%m/%Y")], 
+          ["MONTO PRESTAMO",Dinero.to_money(@credit.monto_solicitud)], 
+          ["MONTO A PAGAR", Dinero.to_money(monto_a_pagar)], 
+          ["PAGO FIJO", Dinero.to_money(monto_a_pagar/@credit.product.numero_de_pagos_a_realizar)], 
+          #["TASA INTERES","#{'%.2f' % @credit.product.taza_de_interes_ordinaria}%"], 
+          ["TASA INTERES","#{'%.2f' % @credit.taza_ord}%"], 
+          ["PLAZO "," #{@credit.product.numero_de_pagos_a_realizar} #{@credit.product.etiqueta_plural}"],  
+          ["PERIODICIDAD", @credit.product.payout.periocidad], 
+          ["CAT SIN IVA","#{'%.2f' % @credit.cat_sin_iva}%"]],:cell_style => { size: 8 })
+    move_down 20
+    table(@arreglo,:cell_style => { size: 8 })
 
   end
 end
