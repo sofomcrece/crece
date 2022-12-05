a982a976a37b995855a86205a176ed1cc81db64a
adrianglezlop <entornoc9@gmail.com>
2022-12-05 14:39:02 -0700

    consulta_cred1

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -47,6 +47,29 @@ class ReportsController < ApplicationController
     end
   end
 
+  def consulta_creditos
+    respond_to do |format|
+        format.html {  }
+        format.xlsx { 
+            tipo_padre = params[:tipo]
+            padre_id = params[:id]
+            branch_office_id = params[:sucursal_id]
+            @branch_office = BranchOffice.find(branch_office_id) unless params[:sucursal_id].nil? or params[:sucursal_id]==""
+            unless  params[:tipo].nil? or  params[:tipo]=="" or  params[:id].nil? or  params[:id]==""
+                if tipo_padre.to_i == 1
+                    @padre = Agent.find(padre_id)
+                else
+                    @padre = Company.find(padre_id)
+                end
+            end
+            @customers = Customer.all
+            @customers = @customers.where("customers.agente_empresa = ? and customers.referencia_agente_empresa = ? ",tipo_padre,padre_id) unless  params[:tipo].nil? or  params[:tipo]=="" or  params[:id].nil? or  params[:id]==""
+            @customers = Customer.get_by_branch_office(@customers,@branch_office) unless params[:sucursal_id].nil? or params[:sucursal_id]==""
+            @customers = @customers.order(:updated_at)  unless @customers == []   
+        }
+    end
+  end
+
   def num_tablero
       @fecha= params[:fecha].to_date unless params[:fecha].nil?  or params[:fecha] == ""
       @sucursales = BranchOffice.all.order("clave")
