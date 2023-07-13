374e2d542b7f8e0bc269882e9e32054ce7662aaf
sofomcrece <sistemas@financieramarjo.com.mx>
2023-07-12 18:27:19 -0600

    Contrato nuevo11

--- app/pdf/contrato_pdf.rb
+++ app/pdf/contrato_pdf.rb
@@ -96,55 +96,84 @@ class ContratoPdf < Prawn::Document
     text "Cuando el importe de los pagos anticipados no fuere suficiente para amortizar el saldo insoluto en su totalidad, “LA ACREDITANTE” deberá reducir el monto de los pagos periódicos pendientes, salvo cuando pacte con “EL ACREDITADO” que se disminuya el número de pagos a realizar. “LA ACREDITANTE” deberá calcular el importe de los intereses por devengar, con base en el nuevo saldo insoluto.",:size =>10,:align => :justify
     text " "
     text "Cada vez que “EL ACREDITADO” efectúe un pago anticipado, “LA ACREDITANTE” deberá entregarle un comprobante de dicho pago, así como la tabla de amortización correspondiente, junto con el estado de cuenta siguiente. Tratándose de pagos anticipados por un importe igual al saldo insoluto, “LA ACREDITANTE” además del comprobante del pago, deberá entregar o mantener a disposición de “EL ACREDITADO”, el estado de cuenta o documento en el que conste el fin de la relación contractual y la inexistencia de adeudos derivados exclusivamente de dicha relación, dentro de 10 (diez) días hábiles a partir de que se hubiera realizado el pago de los adeudos o en la siguiente fecha para cálculo de intereses que se indica en la carátula bajo el rubro “fecha de corte”.",:size =>10,:align => :justify
+    text " "
     text "Terminado el contrato, si llegara a existir saldo a favor de “EL ACREDITADO” este le será entregado en la fecha en que se dé por terminada la relación y en caso de que “EL ACREDITADO” no acuda a la sucursal, “LA ACREDITANTE” le informará que el saldo se encuentra a su disposición y le será devuelto mediante pago en ventanilla.",:size =>10,:align => :justify
+    text " "
     text "“LA ACREDITANTE” deberá reportar a las sociedades de información crediticia que la cuenta está cerrada sin adeudo alguno dentro del plazo que para tales efectos señala la ley para regular las sociedades de información crediticia.",:size =>10,:align => :justify
+    text " "
     text "PAGOS ADELANTADOS. “LA ACREDITANTE” podrá recibir pagos por adelantado con el fin de aplicarlos a cubrir pagos periódicos inmediatos siguientes del crédito, siempre que medie una solicitud por escrito de “EL ACREDITADO”. Cuando el importe del pago sea superior al que deba cubrirse en un periodo, “EL ACREDITADO” deberá de autorizar que los recursos que se entregan en exceso a sus obligaciones exigibles no se apliquen para el pago anticipado del principal, si no como pagos adelantados, lo anterior a través de un escrito con firma autógrafa que incluya la leyenda siguiente: el usuario autoriza que los recursos que se entregan en exceso a sus obligaciones exigibles, no se apliquen para pagos anticipados del principal, sino que se utilicen para cubrir por adelantado los pagos periódicos del crédito inmediatos siguientes.",:size =>10,:align => :justify
+    text " "
     text "Cuando “LA ACREDITANTE” reciba el pago aun no exigible del periodo o importes inferiores, no será necesario el escrito mencionado en el párrafo anterior.",:size =>10,:align => :justify
+    text " "
     text "Cada vez que “EL ACREDITADO” efectué un pago adelantado “LA ACREDITANTE” entregará o pondrá a disposición de este último, el estado de cuenta o documento en el que consté el pago adelantado y entregará el comprobante correspondiente.",:size =>10,:align => :justify
+    text " "
     text "Cuando la terminación del contrato sea por conducto de otra “ENTIDAD FINANCIERA” esta liquidará el adeudo de “EL ACREDITADO” de acuerdo a la información que proporcione “LA ACREDITANTE” y una vez cubiertos los adeudos esta última renuncia a todos los derechos de cobro remanente que pudieran subsistir después del momento de la cancelación.",:size =>10,:align => :justify
     text " "
     text "TASA DE INTERÉS ORDINARIA",:size =>10,:align => :justify
     text "CUARTA. “EL ACREDITADO” se obliga a pagar a “LA ACREDITANTE” intereses ordinarios sobre el monto del crédito a razón de una tasa fija del % (por ciento) anual, más IVA (Impuesto al Valor Agregado). Convienen las partes en que el interés que se aplique al importe financiero se calculará a partir de la fecha de firma del presente contrato, los mismos se pagarán en las fechas indicadas en la tabla de amortización. El pago de los intereses no podrá ser exigido por adelantado, sino únicamente por períodos vencidos.",:size =>10,:align => :justify
+    text " "
     text "Los intereses ordinarios se calcularán de acuerdo a la siguiente fórmula:",:size =>10,:align => :justify
+    text " "
     text "Multiplicando el monto del crédito autorizado por la tasa anual de interés ordinaria aplicable y el resultado se dividirá entre 360 (trescientos sesenta), obteniendo así el monto diario de interés",:size =>10,:align => :justify
+    text " "
     text "El interés diario se multiplicará por el número de días correspondientes del periodo del crédito.",:size =>10,:align => :justify
+    text " "
     text "La fecha de corte para el cálculo de los intereses se señala en la carátula, la cual forma parte integrante del presente contrato.",:size =>10,:align => :justify
 
     text "INTERESES MORATORIOS.",:size =>10,:align => :justify
     text "QUINTA. En caso de incumplimiento de la obligación de pago, según lo pactado en la cláusula tercera de este contrato el acreditado se obliga a pagar una tasa fija de interés moratorio del % anual, calculada diariamente sobre el capital devengado y no pagado, aplicable desde el día siguiente en que incurrió en mora y hasta el día en que se efectúe el pago total del adeudo.",:size =>10,:align => :justify
+    text " "
     text "Los intereses moratorios se calcularán de acuerdo a la siguiente fórmula:",:size =>10,:align => :justify
+    text " "
     text "Dividiendo la tasa fija anual de interés moratorio entre 360, lo cual dará como resultado la tasa diaria,",:size =>10,:align => :justify
+    text " "
     text "El interés diario se multiplica por el número de días que hayan transcurrido desde el día siguiente a la fecha de pago hasta el día en que se realice el pago (días vencidos) ",:size =>10,:align => :justify
+    text " "
     text "El resultado obtenido se multiplicará por el saldo vencido del crédito",:size =>10,:align => :justify
 
     text "OBLIGACIONES DE “EL ACREDITADO”.",:size =>10,:align => :justify
     text "SEXTA.  Además de las obligaciones contenidas en el presente instrumento, durante la vigencia del presente contrato, el acreditado se obliga a:",:size =>10,:align => :justify
+    text " "
     text "1. Hacer entrega de la copia de la ficha de depósito o comprobante de pago expedido por la institución de crédito, al representante de “LA ACREDITANTE”, al día siguiente hábil de su vencimiento.",:size =>10,:align => :justify
+    text " "
     text "2. Proporcionar en cualquier momento, durante la vigencia del crédito, la información que le sea requerida por “LA ACREDITANTE”.",:size =>10,:align => :justify
+    text " "
     text "3. Informar a “LA ACREDITANTE” de cualquier situación que sea posible causa de vencimiento anticipado del contrato.",:size =>10,:align => :justify 
+    text " "
     text "4.- Dar aviso por escrito de su cambio de domicilio, notificando dicha circunstancia a “LA ACREDITANTE” con por lo menos 30 días de anterioridad a que el cambio de domicilio suceda.",:size =>10,:align => :justify 
+    text " "
     text "5.  Informar oportunamente a “LA ACREDITANTE” cualquier acto o hecho que pueda afectar la recuperación del financiamiento.",:size =>10,:align => :justify 
-    
+    text " "
     text "CAUSAS DE VENCIMIENTO ANTICIPADO.",:size =>10,:align => :justify 
     text "SÉPTIMA “LA ACREDITANTE” se reserva la facultad de dar por vencido anticipadamente el presente contrato y exigir desde luego la totalidad del crédito y sus intereses, sin necesidad de requisito o trámite previo alguno, cuando el acreditado faltaré al cumplimiento de cualquiera de las obligaciones contraídas en éste contrato, además de los casos en que la ley así lo previene y en cualquiera de los siguientes supuestos:",:size =>10,:align => :justify 
+    text " "
     text "1.- Dejare de pagar puntualmente cualquiera de las amortizaciones del crédito, en los términos pactados.",:size =>10,:align => :justify 
+    text " "
     text "2.- Haya proporcionado información falsa para el otorgamiento del crédito.",:size =>10,:align => :justify 
+    text " "
     text "El vencimiento anticipado, tendrá como consecuencia, hacer exigible el pago inmediato del capital insoluto y los intereses ordinarios y moratorios que en su caso se generen, a cargo de “EL ACREDITADO”.",:size =>10,:align => :justify 
-    
+    text " "
     text "COMISION POR APERTURA DE CREDITO.",:size =>10,:align => :justify 
     text "OCTAVA. El presente producto no genera comisión alguna para “EL ACREDITADO” .",:size =>10,:align => :justify 
-    
+    text " "
     text "ESTADOS DE CUENTA, CONSULTA DE SALDOS, TRANSACCIONES Y MOVIMIENTOS",:size =>10,:align => :justify 
     text "NOVENA. “LAS PARTES” acuerdan que “LA ACREDITANTE” pondrá a disposición de “EL ACREDITADO” de forma mensual, dentro de los diez días hábiles siguientes a la fecha de corte, es decir, al último día natural de cada mes copia de su estado de cuenta, de manera gratuita en sus oficinas o sucursales, lo anterior en sustitución de la obligación del envío al domicilio de “EL ACREDITADO”, en el entendido de que “EL ACREDITADO” puede solicitar en cualquier momento el envío del estado de cuenta a su domicilio.  Así mismo “EL ACREDITADO” podrá consultar saldos, transacciones y movimientos en el domicilio de “LA ACREDITANTE”, para tal efecto tendrá que exhibir una identificación oficial vigente.",:size =>10,:align => :justify 
-    
+    text " "
     text "ATENCIÓN A USUARIOS",:size =>10,:align => :justify 
     text "DÉCIMA. Para cualquier aclaración o queja que se relacione con el crédito, “LA ACREDITANTE” pone a disposición de “EL ACREDITADO” la Unidad Especializada de Atención a Usuarios ubicada en Río Nazas No. 1240, Colonia Los Pinos, Código Postal 80128, Culiacán, Sinaloa, con horario de atención de lunes a jueves de 8:00 a 17:00 hrs y viernes de 8:00 a 15:00 hrs. asimismo para cualquier solicitud o consulta que se relacione con el crédito, “EL ACREDITADO” podrá formularla vía telefónica al número de teléfono 667 7660935 o por escrito, ante el titular de la unidad referida al correo electrónico contacto@financieramarjo.com.mx",:size =>10,:align => :justify 
+    text " "
     text "Para efecto de formular cualquier aclaración, consulta (incluida información de saldos, transacciones y movimientos), inconformidad, reclamación o queja con respecto a la información contenida en el estado de cuenta o por cualquier acontecimiento suscitado con motivo de la apertura de crédito materia del presente contrato “EL ACREDITADO” podrá realizarlo a través de la Unidad Especializada de Atención a Usuarios, cuyos datos fueron precisados en el párrafo anterior.",:size =>10,:align => :justify 
+    text " "
     text "El proceso para presentar ante la Unidad Especializada alguna duda, consulta general del producto contratado, consulta de saldos, transacciones, movimientos, aclaración, solicitud, inconformidad, o queja podrá hacerlo en cualquier momento, por escrito con firma autógrafa y previa identificación, manifestando su solicitud o inconformidad y acompañando a la misma el contrato y anexos del  producto o servicio contratado,  así como también una copia de identificación oficial vigente, una vez recibida la anterior documentación se procederá a responderla en los plazos que determine la Ley para la Protección y Defensa al Usuario de Servicios Financieros.",:size =>10,:align => :justify 
+    text " "
     text "Para una aclaración o reclamación, “EL ACREDITADO” cuenta con un plazo de 90 (noventa) días naturales contados a partir del hecho que le dio origen o de la fecha de cálculo de intereses, apegándose al siguiente Procedimiento, previsto en el Artículo 23 de la Ley para la Transparencia y Ordenamiento de los Servicios Financieros, se transcribe el procedimiento de atención previsto en el mismo y al cual se sujeta “LA ACREDITANTE”:",:size =>10,:align => :justify 
+    text " "
     text "I. Cuando el Cliente no esté de acuerdo con alguno de los movimientos que aparezcan en el estado de cuenta respectivo o en los medios electrónicos, ópticos o de cualquier otra tecnología que se hubieren pactado, podrá presentar una solicitud de aclaración dentro del plazo de noventa días naturales contados a partir de la fecha de corte o, en su caso, de la realización de la operación o del servicio. hubieren pactado, podrá presentar una solicitud de aclaración dentro del plazo de noventa días naturales contados a partir de la fecha de corte o, en su caso, de la realización de la operación o del servicio.",:size =>10,:align => :justify 
+    text " "
     text "La solicitud respectiva podrá presentarse ante la sucursal en la que radica la cuenta, o bien, en la unidad especializada de la institución de que se trate, mediante escrito, correo electrónico o cualquier otro medio por el que se pueda comprobar fehacientemente su recepción. En todos los casos, la institución estará obligada a acusar recibo de dicha solicitud.",:size =>10,:align => :justify 
+    text " "
     text "Tratándose de cantidades a cargo del Cliente dispuestas mediante cualquier mecanismo determinado al efecto por la Comisión Nacional para la Protección y Defensa de los Usuarios de los Servicios Financieros en disposiciones de carácter general, el Cliente tendrá el derecho de no realizar el pago cuya aclaración solicita, así como el de cualquier otra cantidad relacionada con dicho pago, hasta en tanto se resuelva la aclaración conforme al procedimiento a que se refiere este artículo;",:size =>10,:align => :justify 
+    text " "
     text "II. Una vez recibida la solicitud de aclaración, la institución tendrá un plazo máximo de cuarenta y cinco días para entregar al Cliente el dictamen correspondiente, anexando copia simple del documento o evidencia considerada para la emisión de dicho dictamen, con base en la información que, conforme a las disposiciones aplicables, deba obrar en su poder, así como un informe detallado en el que se respondan todos los hechos contenidos en la solicitud presentada por el Cliente. En el caso de reclamaciones relativas a operaciones realizadas en el extranjero, el plazo previsto en este párrafo será hasta de ciento ochenta días naturales.",:size =>10,:align => :justify
     text " "
     text "El dictamen e informe antes referidos deberán formularse por escrito y suscribirse por personal de la institución facultado para ello. En el evento de que, conforme al dictamen que emita la institución, resulte procedente el cobro del monto respectivo, el Cliente deberá hacer el pago de la cantidad a su cargo, incluyendo los intereses ordinarios conforme a lo pactado, sin que proceda el cobro de intereses moratorios y otros accesorios generados por la suspensión del pago realizada en términos de esta disposición; ",:size =>10,:align => :justify
