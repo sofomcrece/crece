3522a60e28d8721181266df3d5b2a678ad9b1f76
sofomcrece <sistemas@financieramarjo.com.mx>
2023-07-19 14:38:41 -0600

    contrato11

--- app/pdf/autorizacion_pdf.rb
+++ app/pdf/autorizacion_pdf.rb
@@ -38,9 +38,9 @@ class AutorizacionPdf < Prawn::Document
      text "Autorización para:",:size =>10,:align => :justify 
      text "Persona Física (PF) [ X ] 	 Persona Física con Actividad Empresarial (PFAE) [ ]       Persona Moral (PM) [ ] ",:size =>10,:align => :justify 
      
-     text "Por este conducto autorizo expresamente a Crece Prestaciones, SOFOM, ENR, para que por conducto de sus funcionarios facultados lleve a cabo investigaciones sobre mi comportamiento crediticio o el de la empresa que represento en las Sociedades de Información Crediticia que estime conveniente.",:size =>10,:align => :justify 
+     text "Por este conducto autorizo expresamente a Crece Prestaciones, SAPI de C.V. SOFOM, ENR, para que por conducto de sus funcionarios facultados lleve a cabo investigaciones sobre mi comportamiento crediticio o el de la empresa que represento en las Sociedades de Información Crediticia que estime conveniente.",:size =>10,:align => :justify 
      text " "
-     text "Así mismo, declaro que conozco la naturaleza y alcance de las sociedades de información crediticia y de la información contenida en los reportes de crédito y reporte de crédito especial, declaro que conozco la naturaleza y alcance de la información que se solicitará, del uso que Financiera Marjo, S.A. de C.V., SOFOM, ENR hará de tal información y de que ésta podrá realizar consultas periódicas de mi historial crediticio o el de la empresa que represento, consintiendo que esta autorización se encuentre vigente por un período de 3 años contados a partir de la fecha de su expedición y en todo caso durante el tiempo que mantengamos relación jurídica.",:size =>10,:align => :justify 
+     text "Así mismo, declaro que conozco la naturaleza y alcance de las sociedades de información crediticia y de la información contenida en los reportes de crédito y reporte de crédito especial, declaro que conozco la naturaleza y alcance de la información que se solicitará, del uso que Crece Prestaciones, SAPI de C.V. SOFOM, ENR hará de tal información y de que ésta podrá realizar consultas periódicas de mi historial crediticio o el de la empresa que represento, consintiendo que esta autorización se encuentre vigente por un período de 3 años contados a partir de la fecha de su expedición y en todo caso durante el tiempo que mantengamos relación jurídica.",:size =>10,:align => :justify 
      text " "
      text "En caso de que la solicitante sea una Persona Moral, declaro bajo protesta de decir verdad Ser Representante Legal de la empresa mencionada en esta autorización; manifestando que a la fecha de firma de la presente autorización los poderes no me han sido revocados, limitados, ni modificados en forma alguna",:size =>10,:align => :justify 
           
@@ -57,7 +57,7 @@ class AutorizacionPdf < Prawn::Document
  Nombre del funcionario que recaba la autorización: ___________________________
 "
     text " "
-    text  "Estoy consciente y acepto que este documento quede bajo custodia de Financiera Marjo, S.A. de C.V., SOFOM, ENR y/o Sociedad de Información Crediticia consultada para efectos de control y cumplimiento del artículo 28 de la Ley para Regular a Las Sociedades de Información Crediticia; mismo que señala que las Sociedades sólo podrán proporcionar información a un Usuario, cuando éste cuente con la autorización expresa del Cliente mediante su firma autógrafa.
+    text  "Estoy consciente y acepto que este documento quede bajo custodia de Crece Prestaciones, SAPI de C.V. SOFOM, ENR y/o Sociedad de Información Crediticia consultada para efectos de control y cumplimiento del artículo 28 de la Ley para Regular a Las Sociedades de Información Crediticia; mismo que señala que las Sociedades sólo podrán proporcionar información a un Usuario, cuando éste cuente con la autorización expresa del Cliente mediante su firma autógrafa.
 ",:size =>10,:align => :justify 
   text " "
   text "___________________________________________________________________
@@ -65,13 +65,12 @@ Nombre y Firma de PF, PFAE o Representante Legal de la empresa
  ",:size =>10,:align => :justify 
   text " "
   text "___________________________________________________________________________________________________________
-Para uso exclusivo de Crece Prestaciones, SOFOM, ENR
+Para uso exclusivo de Crece Prestaciones, SAPI de C.V. SOFOM, ENRs
 Clave de usuario:_______________________________________
 Fecha de la consulta (dd-mm-aaaa):______________________
 Folio de consulta (Obligatorio):________________________
 Firma: ________________________________________________________________",:size =>10,:align => :justify
 text"IMPORTANTE: Este formato debe ser llenado individualmente, para una sola persona física ó para una sola empresa. En caso de requerir el Historial crediticio del representante legal, favor de llenar un formato adicional.",:size =>10,:align => :justify 
-text"__________________________________________________________________________________________________________
 #{Conf.last.domicilio} TEL. #{Conf.last.telefono}
 CULIACÁN DE ROSALES, SINALOA #{Conf.last.correo}",:size =>10,:align => :justify 
   
