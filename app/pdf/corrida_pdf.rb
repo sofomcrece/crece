bca5544a95ddf20742a2dc12ec3a9d6e1aca5844
sofomcrece <sistemas@financieramarjo.com.mx>
2023-07-26 11:11:57 -0600

    cambios corrida

--- app/pdf/corrida_pdf.rb
+++ app/pdf/corrida_pdf.rb
@@ -14,7 +14,7 @@ class CorridaPdf < Prawn::Document
   def text_content
    #monto_a_pagar= @credit.monto_solicitud*(1+(@credit.product.taza_de_interes_ordinaria/100))
    monto_a_pagar= @credit.monto_solicitud*(1+(@credit.taza_ord/100))
-    image "#{Rails.root}/app/assets/images/logo crece.png", width: 155, height: 85s
+    image "#{Rails.root}/app/assets/images/logo crece.png", width: 155, height: 85
     text "#{@credit.nombre_completo_deudor}"
     table([["FECHA DE PRESTAMO", @credit.fecha_de_contrato.strftime("%d/%m/%Y")], 
           ["MONTO PRESTAMO",Dinero.to_money(@credit.monto_solicitud)], 
@@ -29,8 +29,11 @@ class CorridaPdf < Prawn::Document
     table(@arreglo,:cell_style => { size: 8 })
     text " "
     text "CUENTAS BANCARIAS PARA REALIZAR SUS PAGOS"
-    text "A NOMBRE DE: CRECE PRESTACIONES S.A.P.I. DE C.V., SOFOM, ENR"
-    table([["BANCO","CUENTA","CLABE","No. TARJETA"])
+    text "A NOMBRE DE: FINANCIERA MARJO, S.A. DE C.V., SOFOM, ENR"
+    table([["BANCO","CUENTA","CLABE","No. TARJETA"],
+          ["BANORTE","0857315292","072730008573152921",""],
+          ["INBURSA","50060343855","036730500603438554","4658 2858 0047 5869"],
+          ["BAJIO","112248540201","030730900002594213",""]],:cell_style => { size: 8 })
     start_new_page 
 
     image "#{Rails.root}/app/assets/images/logo crece.png", width: 155, height: 85
